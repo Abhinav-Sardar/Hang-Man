fae3338fe86b623790d428e716a20f6d9e0b1731
Abhinav-Sardar <abhinavsardar.podar@gmail.com>
2021-05-16 21:29:44 +0530

    Added Button Functionality

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 "use strict";
 let words = 'Apple Banana Lake Night Dumpling Scam Prize Lotus Computer Desktop Laptop Doctor Roll Cross Cream Cash Fish Trace Candy Chocolate Dimple Campaign Cookie Defence Protection Trees Beauty Scroll Shoes Fuss Trumpet Butter Ice Sauce Guitar Keyboard Bird Tiger Lion Tongue Spear Spare Cactus Mountain God Chicken zebra violin dog skin shelter food ear lungs ball bat cucumber lolipop star tractor plank log treat'.toUpperCase();
 let currentImageIndex = 0;
+let divs = document.getElementsByClassName('letter');
 let image = document.getElementById('img');
-const ImageArray = ['./Images/Initial.png', './Images/Head.png', './Images/Body.png', './Images/LeftHand.png', './Images/RightHand.png', './Images/LeftLeg.png', './Images/RightLeg.png'];
+const ImageArray = ['./Images/Initial.png', './Images/Head.png', './Images/Body.png', './Images/RightHand.png', './Images/LeftHand.png', './Images/LeftLeg.png', './Images/RightLeg.png'];
 let wordArray = words.split(' ');
 let word = "";
 let splitWord = "";
@@ -34,17 +35,48 @@ document.addEventListener('DOMContentLoaded', () => {
     setButtons();
     setWord();
 });
-setInterval(() => {
-    image === null || image === void 0 ? void 0 : image.setAttribute('src', ImageArray[currentImageIndex]);
-});
-function ButtonClick(button) {
+let ButtonClick = (button) => {
     let btnText = button.innerText;
     if (splitWord.includes(btnText)) {
-        console.log(true);
+        splitWord = ReplaceButtonText(splitWord, btnText);
+        console.log(splitWord);
     }
     else {
-        console.log(false);
+        currentImageIndex++;
     }
+    button.style.display = 'none';
+};
+function ReplaceButtonText(splittedWord, character) {
+    let charArr = [];
+    let divUnderLineArr = [];
+    let newSplitWord = "";
+    for (let index = 0; index < splittedWord.length; index++) {
+        if (splittedWord[index] === character) {
+            charArr.push(splittedWord[index]);
+        }
+        else {
+            newSplitWord += splittedWord[index];
+        }
+    }
+    for (let index = 0; index < word.length; index++) {
+        if (word[index] === charArr[0]) {
+            divs[index].innerText = charArr[0];
+        }
+        else {
+            continue;
+        }
+    }
+    return newSplitWord;
+    // At this point , we have :- 
+    /**
+     * charArr = ['C' , 'C' , 'C'] ;
+     *  splitWord = newSplitWord ;
+     * CoCOCO
+     * if(currentWord === word[index]){
+     * DIVS[i].innetText = currentArr[index] ;
+     * }
+     *
+     */
 }
-function ReplaceButtonText(word, character) {
-}
+setInterval(() => {
+});
--- script.ts
+++ script.ts
@@ -1,9 +1,9 @@
 let words = 'Apple Banana Lake Night Dumpling Scam Prize Lotus Computer Desktop Laptop Doctor Roll Cross Cream Cash Fish Trace Candy Chocolate Dimple Campaign Cookie Defence Protection Trees Beauty Scroll Shoes Fuss Trumpet Butter Ice Sauce Guitar Keyboard Bird Tiger Lion Tongue Spear Spare Cactus Mountain God Chicken zebra violin dog skin shelter food ear lungs ball bat cucumber lolipop star tractor plank log treat'.toUpperCase() ; 
 
 let currentImageIndex = 0 ; 
-
+let divs = document.getElementsByClassName('letter') ; 
 let image = document.getElementById('img') ; 
-const ImageArray = ['./Images/Initial.png' , './Images/Head.png' , './Images/Body.png' , './Images/LeftHand.png' , './Images/RightHand.png' , './Images/LeftLeg.png' , './Images/RightLeg.png'] ; 
+const ImageArray = ['./Images/Initial.png' , './Images/Head.png' , './Images/Body.png' , './Images/RightHand.png' , './Images/LeftHand.png' , './Images/LeftLeg.png' , './Images/RightLeg.png'] ; 
 
 let wordArray = words.split(' ') ; 
 let word = "" ; 
@@ -40,20 +40,54 @@ document.addEventListener('DOMContentLoaded' , () => {
     setWord() ; 
 }) ; 
 
-setInterval(() => {
-    image?.setAttribute('src' , ImageArray[currentImageIndex]) ; 
-}) ;
 
-function ButtonClick(button:HTMLButtonElement) {
+
+let ButtonClick:(btton:HTMLButtonElement) => void =  (button:HTMLButtonElement) => {
     let btnText = button.innerText ; 
     if(splitWord.includes(btnText)){
-        console.log(true)
+        splitWord = ReplaceButtonText(splitWord , btnText) ; 
+        console.log(splitWord)
     }
     else {
-        console.log(false)
+        currentImageIndex++ ; 
     }
+    button.style.display = 'none' ;
+}
+function ReplaceButtonText(splittedWord:string, character:string ){
+    let charArr = [] ; 
+    let divUnderLineArr = [] ; 
+    let newSplitWord = "" ; 
+    for (let index = 0; index < splittedWord.length; index++) {
+        if(splittedWord[index] === character){
+            charArr.push(splittedWord[index]); 
+        }
+        else{
+            newSplitWord += splittedWord[index] ; 
+        }
+    }
+    for (let index = 0; index< word.length; index++) {
+        if(word[index] === charArr[0]){
+            divs[index].innerText = charArr[0] ;
+        }
+        else {
+            continue ; 
+        }
+        
+    }
+    return newSplitWord;
+    // At this point , we have :- 
+    /**
+     * charArr = ['C' , 'C' , 'C'] ; 
+     *  splitWord = newSplitWord ; 
+     * CoCOCO 
+     * if(currentWord === word[index]){
+     * DIVS[i].innetText = currentArr[index] ;
+     * }
+     * 
+     */
 
 }
-function ReplaceButtonText(word:string, character:string ){
 
-}
+setInterval(() => {
+   
+}) ;
